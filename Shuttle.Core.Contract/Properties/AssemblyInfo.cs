995cea7098bae125fb0622b9e6359d32d3a4921c
Eben <me@ebenroux.co.za>
2024-10-14 20:25:25 +0200

    v20 build

--- Shuttle.Core.Contract/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Contract/Properties/AssemblyInfo.cs
@@ -1,14 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if NETSTANDARD
-[assembly: AssemblyTitle(".NET Standard")]
-#endif
-
-#if NET6_0_OR_GREATER
 [assembly: AssemblyTitle(".NET Unified Platform")]
-#endif
-
 [assembly: AssemblyVersion("20.0.0.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2024, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Core.Contract")]
